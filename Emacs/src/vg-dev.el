71e322ea67c4220648cc0a0d5c3ed4d26352ed06
vincent.greff <vince@greffsite.com>
2020-05-11 17:19:01 -0500

    added root and config dir

--- Emacs/src/vg-dev.el
+++ Emacs/src/vg-dev.el
@@ -108,9 +108,13 @@
 
 (use-package helm)
 
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action)
+
 (global-set-key (kbd "M-x") #'helm-M-x)
 (global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks)
 (global-set-key (kbd "C-x C-f") #'helm-find-files)
+(global-set-key (kbd "C-x b") 'helm-buffers-list)
+(global-set-key (kbd "M-y") 'helm-show-kill-ring)
 (helm-mode 1)
 
 (use-package helm-lsp
